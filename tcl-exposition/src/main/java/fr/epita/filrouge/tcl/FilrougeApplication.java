66e9c54b07cda2ebe2e3f0c9579c2adfa42d40cc
Fabien Laurette <fabien.laurette@gmail.com>
2021-02-12 19:30:06 +0100

    1st commit

---
+++ tcl-exposition/src/main/java/fr/epita/filrouge/tcl/FilrougeApplication.java
@@ -0,0 +1,16 @@
+package fr.epita.filrouge.tcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilrougeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilrougeApplication.class, args);
+
+        System.out.println("Let's start ! ");
+
+    }
+
+}
